5f70862e6f1ec43648d9ba59b1d8608481cc64ef
ESATA-xiaoxin <1324225011@qq.com>
2022-04-12 22:49:13 +0800

    x-commit

---
+++ demo1.py
@@ -0,0 +1,24 @@
+# 用   户:张文薪
+# 开发时间: 2021/2/15 12:57
+print("helloworld")
+
+print(520)
+print(1314)
+print(521)
+
+
+print(521)
+
+print(520)
+
+print(5+1)
+print('helloworld')
+
+
+fp=open('D:/text.txt',"a+")
+print('helloworld',file=fp)#注意file=fp,fp理解为变量，a也可代替
+fp.close()#()可有可无
+
+
+print('hello','world','python')
+print('你好')
