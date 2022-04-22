bb0e61a0d87e6642ef6431892b5dbcdc9af98136
ESATA-xiaoxin <1324225011@qq.com>
2022-04-22 13:21:26 +0800

    x-commit

---
+++ chap2/demo7.py
@@ -0,0 +1,16 @@
+# 用   户:张文薪
+# 开发时间: 2021/2/16 11:33
+str1='人生苦短，我用Pytnon'
+str2="人生苦短，我用Pytnon"
+str3="""人生苦短，
+我用Pytnon"""
+
+str4='''人生苦短，
+我用Pytnon'''
+print(str1,type(str1))
+print(str2,type(str2))
+print(str3,type(str3))
+print(str4,type(str4))
+str5='人生苦短,' \
+     '我用Pytnon'
+print(str5,type(str5))
