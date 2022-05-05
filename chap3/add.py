b61eb28e303cc448b63fde01f126036880776261
ESATA-xiaoxin <1324225011@qq.com>
2022-05-05 15:18:58 +0800

    x-commit

---
+++ chap3/add.py
@@ -0,0 +1,22 @@
+# 用   户:张文薪
+# 开发时间: 2021/2/17 10:44
+#从键盘录入两个整数，计算两个整数的和
+
+m=input('请输入一个加数：')
+n=input('请输入另一个加数')
+print(type(m),type(n))
+print(m+n)
+
+
+
+
+
+
+a=int(input('请输入一个加数：'))
+#a=int(a)  #将转换之后的结果存储到a中
+b=int(input('请输入另一个加数：'))
+#b=int(b)
+print(type(a),type(b))
+print(a+b)
+
+
